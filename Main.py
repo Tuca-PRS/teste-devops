90303024f2c503babee816fb71b0d88ef6f9a206
Tuca-PRS <paulorodrigodesouza@live.com>
2024-09-08 20:32:33 -0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -8,7 +8,7 @@ async def root():
     return {"message": "Hello World"}
 
 #127.0.0.1:8000/teste1
-app.get("/teste1")
+app.get("/teste")
 async def funcaoteste():
     return {"teste": "Deu certo!!"}
 
